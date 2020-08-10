e05834c577d205fdadc5d2c0198696bcd904b294
zybrik0007 <zybrik0007@rambler.ru>
2020-08-10 14:14:15 +0300

    Работа на два ПК, неважные коммиты.

--- src/app/administration/validators/validator-administration.ts
+++ src/app/administration/validators/validator-administration.ts
@@ -46,7 +46,7 @@ export class ValidatorAdministration {
   }
 
   static inbreed(control: FormControl): { [key: string]: boolean } {
-    if (control.value !== null) {
+    if (control.value.trim() !== '') {
       if (!Number.isInteger(control.value)) {
         return {
           inbreed: true
