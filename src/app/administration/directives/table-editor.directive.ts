198d42421c7f897071ca4d24afe282cc0bc6805c
zybrik0007 <phone627781>
2020-08-12 14:26:58 +0300

    Разработка на два ПК. Неважный Коммит.

--- src/app/administration/directives/table-editor.directive.ts
+++ src/app/administration/directives/table-editor.directive.ts
@@ -5,9 +5,20 @@ import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
 })
 export class TableEditorDirective {
 
-  constructor(private element: ElementRef, private renderer: Renderer2) { }
+  constructor(private element: ElementRef,
+              private renderer: Renderer2
+              ) {}
 
   @HostListener('click') onMouseClick() {
+    const test = this.element.nativeElement;
+    const data = test.dataset;
+    if (data['active'] === 'disable') {
+      this.element.nativeElement.dataset['active'] = 'active';
+      this.renderer.setStyle(this.element.nativeElement, 'background', '#816783');
+    } else {
+      this.element.nativeElement.dataset['active'] = 'disable';
+      this.renderer.setStyle(this.element.nativeElement, 'background', '');
+    }
     console.log(this);
   }
 
